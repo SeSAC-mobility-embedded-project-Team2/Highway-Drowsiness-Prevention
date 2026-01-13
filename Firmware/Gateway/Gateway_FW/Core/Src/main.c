172f8d41a4e6fc6dea7ef8a92b79d5f51066904f
yonguk98 <kimyu9827@naver.com>
2026-01-13 23:02:28 +0900

    CABIN-6 test commit1

--- Firmware/Gateway/Gateway_FW/Core/Src/main.c
+++ Firmware/Gateway/Gateway_FW/Core/Src/main.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+// test comment
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
